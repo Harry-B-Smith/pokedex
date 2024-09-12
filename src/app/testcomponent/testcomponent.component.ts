9fafc5a8b0afe9aa3fe713e1dae439c324984521
Harry-B-Smith <harrybsmithiv@gmail.com>
2024-09-11 21:10:56 -0400

    create search box and dbounce timer

--- src/app/testcomponent/testcomponent.component.ts
+++ src/app/testcomponent/testcomponent.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-testcomponent',
   standalone: true,
   templateUrl: './testcomponent.component.html',
-  styleUrl: './testcomponent.component.css'
+  styleUrl: './testcomponent.component.css',
+  imports: [FormsModule]
 })
 export class TestcomponentComponent implements OnInit {
 
+  public inputBox = "pikachu";
+  public timer: any;
 
   constructor(private http:HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get<any>("https://pokeapi.co/api/v2/pokemon/ditto").subscribe(value => console.log(value));
+    this.search("ditto");
+  }
+
+   search(pokemon: String){
+    console.log(pokemon);
+    this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .subscribe(value => console.log(value));
+  }
+
+  log(value: String) {
+    console.log(value);
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.search(value), 3000);
   }
 
 
