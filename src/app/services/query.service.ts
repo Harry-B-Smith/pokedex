b9bcc4ea4161c1817a2c377646805496f7bdcbdb
Harry-B-Smith <harrybsmithiv@gmail.com>
2024-10-23 21:10:17 -0400

    add styling and service comments with todos

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -8,17 +8,21 @@ import { debounceTime, map, Subject, switchMap } from 'rxjs';
 export class QueryService implements OnInit
 {
 
-  public pokemon = '';
+  public pokemon?: string;
   public pokemon_type = '';
   public imageUrl = "";
   public height = '';
   public weight = '';
 
+    // recent search results
+  // search caching
+  // auto completion
+  // formify angular form
+
   constructor(private http:HttpClient, ) {
     this.searchSubject.pipe(
       debounceTime(3000),
       switchMap(pokemon => {
-        console.log("hello fucker");
         let res = this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         return res;
       })
